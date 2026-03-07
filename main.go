8804d0e956a065393c8416407b3055a5e9234679
Hemakeshwar <hemakeshwargo@gmail.com>
2026-03-07 17:11:48 +0530

    Fixed cli for empty file command

--- main.go
+++ main.go
@@ -1,24 +1,43 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/urfave/cli/v3"
-	"log"
-	"os"
+ "context"
+ "log"
+ "os"
+
+ "github.com/urfave/cli/v3"
+ "funk/commands"
 )
 
 func main() {
-	cmd := &cli.Command{
-		Name:  "funk",
-		Usage: "make an explosive entrance",
-		Action: func(context.Context, *cli.Command) error {
-			fmt.Println("boom! I say!")
-			return nil
-		},
-	}
 
-	if err := cmd.Run(context.Background(), os.Args); err != nil {
-		log.Fatal(err)
-	}
+ cmd := &cli.Command{
+  Name:  "funk",
+  Usage: "Developer CLI tool",
+
+  Commands: []*cli.Command{
+   {
+    Name:  "empty",
+    Usage: "Find empty files",
+    Flags: []cli.Flag{
+     &cli.StringFlag{
+      Name:  "path",
+      Value: ".",
+      Usage: "Directory path to scan",
+     },
+    },
+    Action: func(ctx context.Context, c *cli.Command) error {
+
+     path := c.String("path")
+     commands.FindEmptyFiles(path)
+
+     return nil
+    },
+   },
+  },
+ }
+
+ if err := cmd.Run(context.Background(), os.Args); err != nil {
+  log.Fatal(err)
+ }
 }
