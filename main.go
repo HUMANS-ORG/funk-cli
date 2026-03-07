dd5712ac844b49ddc9a5e444c726e2bf6cbc1e22
Hemakeshwar <hemakeshwargo@gmail.com>
2026-03-07 20:23:00 +0530

    Align with cli framework

--- main.go
+++ main.go
@@ -1,43 +1,28 @@
 package main
 
 import (
- "context"
- "log"
- "os"
+	"context"
+	"log"
+	"os"
 
- "github.com/urfave/cli/v3"
- "funk/commands"
+	"funk/commands"
+
+	"github.com/urfave/cli/v3"
 )
 
 func main() {
 
- cmd := &cli.Command{
-  Name:  "funk",
-  Usage: "Developer CLI tool",
+	cmd := &cli.Command{
+		Name:  "funk",
+		Usage: "Developer CLI tool",
 
-  Commands: []*cli.Command{
-   {
-    Name:  "empty",
-    Usage: "Find empty files",
-    Flags: []cli.Flag{
-     &cli.StringFlag{
-      Name:  "path",
-      Value: ".",
-      Usage: "Directory path to scan",
-     },
-    },
-    Action: func(ctx context.Context, c *cli.Command) error {
+		Commands: []*cli.Command{
+			commands.FileEmptyCommand(),
+			// commands.ConvertCommand()  <- teammates plug in here
+		},
+	}
 
-     path := c.String("path")
-     commands.FindEmptyFiles(path)
-
-     return nil
-    },
-   },
-  },
- }
-
- if err := cmd.Run(context.Background(), os.Args); err != nil {
-  log.Fatal(err)
- }
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
