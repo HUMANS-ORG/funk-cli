944ed0bc85c668d4d282e895deae0d926b21bf38
Dinesh M <dinesh.m.webdev@gmail.com>
2026-03-09 22:22:19 +0530

    add operation

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
- 
+ package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage:")
+		fmt.Println("add <task>")
+		fmt.Println("list")
+		fmt.Println("delete <number>")
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+
+	case "add":
+		task := os.Args[2]
+		AddTask(task)
+
+	case "list":
+		ListTasks()
+
+	case "delete":
+		DeleteTask(os.Args[2])
+
+	default:
+		fmt.Println("Unknown command")
+	}
+}
