9be31ebff168ee0594cd72d18bf4fe975a2d54a4
gokulrajk-dev <kgokulraj.developer@gmail.com>
2026-03-10 23:40:21 +0530

    feat: add the timer to the linux

--- commands/commands.go
+++ commands/commands.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os/exec"
 	"github.com/urfave/cli/v3"
+	"runtime"
+	"time"
 )
 
 func TimerCommand() *cli.Command {
@@ -14,15 +16,15 @@ func TimerCommand() *cli.Command {
 		Action: TimerSet,
 		Flags: []cli.Flag{
 			&cli.IntFlag{
-				Name:  "seconds",
+				Name:  "sec",
 				Usage: "timer duration in seconds",
 			},
 			&cli.IntFlag{
-				Name:  "minutes",
+				Name:  "min",
 				Usage: "timer duration in minutes",
 			},
 			&cli.IntFlag{
-				Name:  "hours",
+				Name:  "hr",
 				Usage: "timer duration in hours",
 			},
 		},
@@ -33,34 +35,32 @@ func TimerSet(ctx context.Context, cmd *cli.Command) error {
 
 	var totalSeconds int
 
-
 	switch {
-	case cmd.IsSet("seconds"):
-		totalSeconds = cmd.Int("seconds")
+	case cmd.IsSet("sec"):
+		totalSeconds = cmd.Int("sec")
 
-	case cmd.IsSet("minutes"):
-		totalSeconds = cmd.Int("minutes") * 60
+	case cmd.IsSet("min"):
+		totalSeconds = cmd.Int("min") * 60
 
-	case cmd.IsSet("hours"):
-		totalSeconds = cmd.Int("hours") * 3600
+	case cmd.IsSet("hr"):
+		totalSeconds = cmd.Int("hr") * 3600
 
 	default:
-		return fmt.Errorf("please specify one of: --seconds, --minutes, or --hours")
+		return fmt.Errorf("please specify one of: --sec, --min, or --hr")
 	}
 
 	if totalSeconds <= 0 {
 		return fmt.Errorf("duration must be positive")
 	}
-
-	fmt.Printf("Timer started — %d seconds\n", totalSeconds)
-
 	
-	psCommand := fmt.Sprintf(
+	switch runtime.GOOS {
+	case "windows":
+		fmt.Printf("Timer started — %d seconds\n", totalSeconds)
+		psCommand := fmt.Sprintf(
 		"Start-Sleep -Seconds %d; Import-Module BurntToast; New-BurntToastNotification -Text '%d Seconds Timer Finished'",
 		totalSeconds,totalSeconds,
 	)
 
-	
 	cmdExec := exec.Command(
 		"powershell.exe",
 		"-NoProfile",
@@ -69,11 +69,26 @@ func TimerSet(ctx context.Context, cmd *cli.Command) error {
 	)
 
 	err := cmdExec.Start()
+
 	if err != nil {
 		return fmt.Errorf("failed to start timer: %v", err)
 	}
 
 	fmt.Println("Timer running in background")
 
+	default:
+		for i := totalSeconds ;i>=0;i-- {
+			h:=i/3600
+			m:= (i%3600)/60
+			s:=i % 60
+
+			fmt.Printf("\r⏳ %02d:%02d:%02d", h, m,s)
+
+			time.Sleep(time.Second)
+
+		}
+		fmt.Println("\ntimer finish")
+	}
+	
 	return nil
 }
